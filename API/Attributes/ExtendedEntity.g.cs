907411f534660e4029fbd96526f8dfa1373e2cf7
Pankaj Tiwari <ptiwari@appsnovel.com>
2024-09-26 11:57:25 +0530

    Upload files from CodeZen

---
+++ API/Attributes/ExtendedEntity.g.cs
@@ -0,0 +1,10 @@
+namespace Patient_test
+{
+    /// <summary>
+    /// It will use to indicate ralation
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = false)]
+    public sealed class ExtendedEntity : Attribute
+    {
+    }
+}
---
+++ API/Helpers/BaseApiController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Patient_test.Helpers.Extensions;
+
+namespace Patient_test.Helpers
+{
+    /// <summary>
+/// Base class of API controller
+/// </summary>
+    public class BaseApiController : ControllerBase
+    {
+        /// <summary>
+/// User id
+/// </summary>
+        protected Guid UserId { get => HttpContext.User.Identity.GetUserId(); }
+        /// <summary>
+/// Tenant id
+/// </summary>
+        protected Guid TenantId { get => HttpContext.User.Identity.GetTenantId(); }
+    }
+}
